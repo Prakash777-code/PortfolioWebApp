ff8bf68dccda32b6c5423ae3529f44a2033af449
Prakash Kumar <Prakash Kumar>
2026-06-09 11:14:39 +0530

    Fixed error handling

--- src/Services/contact.tsx
+++ src/Services/contact.tsx
@@ -1,19 +1,24 @@
 export async function submitContactForm(
   name: string,
   email: string,
-  message: string
+  message: string,
 ) {
-  const res = await fetch("/api/contact", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      name,
-      email,
-      message,
-    }),
-  });
+  try {
+    
+    const res = await fetch("/api/contact", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name,
+        email,
+        message,
+      }),
+    });
 
-  return await res.json();
+    return await res.json();
+  } catch (error) {
+    console.log(error);
+  }
 }
